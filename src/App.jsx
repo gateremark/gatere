d9dda67beb1deabe1fbe7da12e6d515aed44c4de
gateremark <mark5gatere@gmail.com>
2023-07-19 12:23:58 +0300

    Added sections, installed tailwindcss, added custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import AnimatedCursor from "react-animated-cursor";
+
 function App() {
 	return (
 		<>
+			<AnimatedCursor
+				innerSize={20}
+				outerSize={20}
+				color="4, 180, 224"
+				outerAlpha={0.2}
+				innerScale={0.7}
+				outerScale={5}
+				clickables={[
+					"a",
+					"img",
+					"Link",
+					'input[type="text"]',
+					'input[type="email"]',
+					'input[type="number"]',
+					'input[type="submit"]',
+					'input[type="image"]',
+					'div[className="projects"]',
+					"label[for]",
+					"select",
+					"textarea",
+					"Particles",
+					"button",
+					".link",
+				]}
+			/>
 			<div>
-				<h1>Latest Portfolio</h1>
+				<h1 className=" text-5xl text-center">Latest Portfolio</h1>
 			</div>
 		</>
 	);
