f227de277e46aed7ff6741e20ce8be2e2df57c98
gateremark <mark5gatere@gmail.com>
2023-10-10 12:27:32 +0300

    added motion in about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -19,13 +19,15 @@ const About = () => {
 			<div className=" pt-28 flex flex-col justify-center items-center font-medium">
 				<div className="md:flex justify-between w-[90%] md:w-[90%] lg:w-[75%]">
 					<div className="w-48 hidden md:w-72 py-[0.3rem] aspect-square rounded-[2rem] bg-gradient-to-tr from-[#690cc600] via-[#430680] to-[#690cc600] hover:bg-[#430680] transition ease-in-out duration-[500ms] md:flex items-center justify-center">
-						<img
-							className="w-48 md:w-72 rounded-[2rem] overflow-hidden rotate-[10deg] hover:rotate-0 transition ease-in-out duration-500"
-							src="https://res.cloudinary.com/dvuazircp/image/upload/v1696751252/new_portfolio/gmark_j4ggfo.webp"
-							alt="Mark Gatere"
-							width="288px"
-							height="438.922px"
-						/>
+						<motion.div variants={fadeIn("tween", 0.2, 0.5)}>
+							<img
+								className="w-48 bg-[#430680] md:w-72 rounded-[2rem] overflow-hidden rotate-[10deg] hover:rotate-0 transition ease-in-out duration-500"
+								src="https://res.cloudinary.com/dvuazircp/image/upload/v1696751252/new_portfolio/gmark_j4ggfo.webp"
+								alt="Mark Gatere"
+								width="288px"
+								height="438.922px"
+							/>
+						</motion.div>
 					</div>
 
 					<div className=" flex flex-col justify-between items-center md:items-start">
@@ -34,15 +36,17 @@ const About = () => {
 							textStyles="text-3xl md:text-4xl font-semibold "
 						/>
 						<div className="md:hidden w-48 py-1 mt-5 md:my-5 md:w-72 aspect-square rounded-[2rem] bg-gradient-to-tr from-[#690cc600] via-[#430680] to-[#690cc600] hover:bg-[#430680] transition ease-in-out duration-[500ms] flex items-center justify-center">
-							<img
-								className="w-48 md:w-72 rounded-[2rem] overflow-hidden rotate-[10deg] hover:rotate-0 transition ease-in-out duration-500"
-								src="https://res.cloudinary.com/dvuazircp/image/upload/v1696751252/new_portfolio/gmark_j4ggfo.webp"
-								alt="Mark Gatere"
-								width="288px"
-								height="438.922px"
-							/>
+							<motion.div variants={fadeIn("tween", 0.2, 0.5)}>
+								<img
+									className="w-48 bg-[#430680] md:w-72 rounded-[2rem] overflow-hidden rotate-[10deg] hover:rotate-0 transition ease-in-out duration-500"
+									src="https://res.cloudinary.com/dvuazircp/image/upload/v1696751252/new_portfolio/gmark_j4ggfo.webp"
+									alt="Mark Gatere"
+									width="288px"
+									height="438.922px"
+								/>
+							</motion.div>
 						</div>
-						<motion.p variants={fadeIn("left", "tween", 0.2, 1)}>
+						<motion.div variants={fadeIn("tween", 0.2, 0.5)}>
 							<div className=" my-2 grid grid-cols-2 gap-5 justify-between items-center">
 								{data.map((item) => (
 									<Card key={item.id} className="">
@@ -65,7 +69,7 @@ const About = () => {
 								• Strong proficiency in JavaScript and a thorough <br />{" "}
 								understanding of ReactJS and its core principles.
 							</p>
-						</motion.p>
+						</motion.div>
 
 						<a
 							className=" mt-4 w-fit flex justify-center gap-2 items-center text-sm md:text-lg text-[#fff] bg-[#690cc6] px-2 py-2 md:py-2 br border-[#690cc6] rounded-md hover:bg-[#380d63] transition ease-in-out duration-500 "
