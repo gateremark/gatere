28a86377f86f770d639353924dd7d7986c9ac61c
gateremark <mark5gatere@gmail.com>
2023-10-13 13:42:26 +0300

    updated Work Data

--- src/components/WorkData.jsx
+++ src/components/WorkData.jsx
@@ -56,4 +56,17 @@ export const workData = [
 			"Participating in code reviews and providing constructive feedback to other developers.",
 		],
 	},
+	{
+		title: "React.js Developer",
+		company_name: "Tesla",
+		icon: tesla,
+		iconBg: "#383E56",
+		date: "March 2020 - April 2021",
+		points: [
+			"Developing and maintaining web apps using React.js and other related technologies.",
+			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+			"Implementing responsive design and ensuring cross-browser compatibility.",
+			"Participating in code reviews and providing constructive feedback to other developers.",
+		],
+	},
 ];
