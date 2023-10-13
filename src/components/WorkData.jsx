8cc4e2d3cf7a173bfcc171648ef48977f6586e35
gateremark <mark5gatere@gmail.com>
2023-10-13 13:08:34 +0300

    created a separate WorkCard component

--- src/components/WorkData.jsx
+++ src/components/WorkData.jsx
@@ -11,7 +11,7 @@ export const workData = [
 		iconBg: "#383E56",
 		date: "March 2020 - April 2021",
 		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
+			"Developing and maintaining web apps using React.js and other related technologies.",
 			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
 			"Implementing responsive design and ensuring cross-browser compatibility.",
 			"Participating in code reviews and providing constructive feedback to other developers.",
