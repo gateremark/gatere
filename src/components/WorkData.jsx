0751c4efa118fbe7de4321ec7e16c3c1173ef085
gateremark <mark5gatere@gmail.com>
2023-10-13 21:18:26 +0300

    updated WorkData

--- src/components/WorkData.jsx
+++ src/components/WorkData.jsx
@@ -1,17 +1,12 @@
-import starbucks from "../assets/company/starbucks.png";
-import tesla from "../assets/company/tesla.png";
-import shopify from "../assets/company/shopify.png";
-import meta from "../assets/company/meta.png";
-
 export const workData = [
 	{
 		title: "React.js Developer",
 		company_name: "Starbucks",
-		icon: starbucks,
+		icon: "https://res.cloudinary.com/dvuazircp/image/upload/v1697220933/starbucks_rvw1dt.webp",
 		iconBg: "#383E56",
 		date: "March 2020 - April 2021",
 		points: [
-			"Developing and maintaining web apps using React.js and other related technologies.",
+			"Developing and maintaining web applications using React.js and other related technologies.",
 			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
 			"Implementing responsive design and ensuring cross-browser compatibility.",
 			"Participating in code reviews and providing constructive feedback to other developers.",
@@ -20,7 +15,7 @@ export const workData = [
 	{
 		title: "React Native Developer",
 		company_name: "Tesla",
-		icon: tesla,
+		icon: "https://res.cloudinary.com/dvuazircp/image/upload/v1697220933/tesla_shlymo.webp",
 		iconBg: "#E6DEDD",
 		date: "Jan 2021 - Feb 2022",
 		points: [
@@ -33,7 +28,7 @@ export const workData = [
 	{
 		title: "Web Developer",
 		company_name: "Shopify",
-		icon: shopify,
+		icon: "https://res.cloudinary.com/dvuazircp/image/upload/v1697220933/shopify_c90yun.webp",
 		iconBg: "#383E56",
 		date: "Jan 2022 - Jan 2023",
 		points: [
@@ -46,7 +41,7 @@ export const workData = [
 	{
 		title: "Full stack Developer",
 		company_name: "Meta",
-		icon: meta,
+		icon: "https://res.cloudinary.com/dvuazircp/image/upload/v1697220933/meta_v3qda5.webp",
 		iconBg: "#E6DEDD",
 		date: "Jan 2023 - Present",
 		points: [
@@ -58,8 +53,8 @@ export const workData = [
 	},
 	{
 		title: "React.js Developer",
-		company_name: "Tesla",
-		icon: tesla,
+		company_name: "Microsoft",
+		icon: "https://res.cloudinary.com/dvuazircp/image/upload/v1697220933/ms_yixnxm.webp"a,
 		iconBg: "#383E56",
 		date: "March 2020 - April 2021",
 		points: [
