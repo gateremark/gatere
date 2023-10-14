e7a647917ef56926c8fa92c8af290fc9fa54ad0a
gateremark <mark5gatere@gmail.com>
2023-10-14 14:00:26 +0300

    added resume link

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,7 +6,9 @@ const Cta = () => {
 			<div className="flex justify-around gap-4 md:gap-10">
 				<a
 					className="inline-block text-sm md:text-lg text-[#690cc6] px-2 py-3 md:py-3 md:px-5 br border-b-[3px] rounded-md hover:bg-[#380d63] hover:text-[#fff] transition ease-in-out duration-500"
-					href=""
+					href="https://drive.google.com/file/d/1vGSJHXNGGTao-JoUGLumbKljMVe1dKEK/view?usp=drive_link"
+					target="_blank"
+					rel="noreferrer"
 				>
 					Download Resume
 				</a>
