cd0e1054307599170f2f54ada509f7471cc95259
gateremark <mark5gatere@gmail.com>
2023-10-20 00:10:36 +0300

    updated resume link

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,17 +6,15 @@ const Cta = () => {
 			<div className="flex justify-around gap-4 md:gap-10">
 				<a
 					className="inline-block text-sm md:text-lg text-[#690cc6] px-2 py-3 md:py-3 md:px-5 br border-b-[3px] rounded-md hover:bg-[#380d63] hover:text-[#fff] transition ease-in-out duration-500"
-					href="https://drive.google.com/file/d/1vGSJHXNGGTao-JoUGLumbKljMVe1dKEK/view?usp=drive_link"
+					href="https://drive.google.com/file/d/1nzCDDQOAxzL8FIi5dI0nWgJT8rvEiqk2/view?usp=share_link"
 					target="_blank"
 					rel="noreferrer"
-					
 				>
 					Download Resume
 				</a>
 				<a
 					className="inline-block text-sm md:text-lg text-[#fff] bg-[#690cc6] px-2 py-3 md:py-3 md:px-5 br border-[#690cc6] rounded-md hover:bg-[#380d63] transition ease-in-out duration-500 "
 					href="#contact"
-					
 				>
 					Let's Talk
 				</a>
