bf1e3f88324023cb1642b2ad60853d18fc63cfee
gateremark <mark5gatere@gmail.com>
2023-12-11 12:49:52 +0300

    added stars in blog page

--- src/sections/ErrorPage.jsx
+++ src/sections/ErrorPage.jsx
@@ -10,7 +10,7 @@ const ErrorPage = () => {
                 className="flex absolute justify-center items-center text-[#430680] top-5 md:top-10 left-5 md:left-20 gap-3 hover:scale-110 transition ease-in-out duration-200"
             >
                 <GiReturnArrow className="text-xl sm:text-3xl md:text-5xl" />
-                <span className="text-xl sm:text-2xl md:text-4xl ">Home</span>
+                <span className="text-xl sm:text-2xl md:text-4xl ">Homepage</span>
             </Link>
             <img src={error} alt="404 Error" className="mt-14 md:mt-0" />
         </div>
