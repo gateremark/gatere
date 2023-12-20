daf19218994b7e496027ca9d6d9563d5efe6e741
gateremark <mark5gatere@gmail.com>
2023-12-20 22:28:24 +0300

    refactor: update resume url

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,7 +6,7 @@ const Cta = () => {
             <div className="flex justify-around gap-4 md:gap-10">
                 <a
                     className="inline-block text-sm md:text-lg text-[#430680] px-2 py-3 md:py-3 md:px-5 br border-b-[3px] rounded-md hover:bg-[#380d63] hover:text-[#fff] transition ease-in-out duration-500"
-                    href="https://drive.google.com/file/d/1cbKXGu7LOQ_F14CQz9ny-3DqKY2UeT9R/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1r26v6WiOlpTBKMzagAlILNI7xzklht4O/view?usp=sharing"
                     target="_blank"
                     rel="noreferrer"
                 >
