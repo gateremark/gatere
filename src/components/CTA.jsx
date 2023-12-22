341703d071ea60cf9a3438f270d0cc4c4bc29316
gateremark <mark5gatere@gmail.com>
2023-12-22 19:43:15 +0300

    update resume link

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,7 +6,7 @@ const Cta = () => {
             <div className="flex justify-around gap-4 md:gap-10">
                 <a
                     className="inline-block text-sm md:text-lg text-[#01f53f] px-2 py-3 md:py-3 md:px-5 border-[#01f53f] border-solid border rounded-md hover:bg-[#00701c] hover:border-[#00701c] hover:text-[#0B1120] transition ease-in-out duration-500"
-                    href="https://drive.google.com/file/d/1s9P87dURW58p_9WDzczvnYqQ3y20OuT7/view?usp=drive_link"
+                    href="https://drive.google.com/file/d/1_tPDnapu_a9poEAU0zMKOUD4sCkkxI0z/view?usp=drive_link"
                     target="_blank"
                     rel="noreferrer"
                 >
