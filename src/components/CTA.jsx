357b88b9b6234516e1a4606789f42c6e67941ff6
gateremark <mark5gatere@gmail.com>
2024-04-18 19:30:37 +0300

    refactor: update resume link

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,7 +6,7 @@ const Cta = () => {
             <div className="flex justify-around gap-4 md:gap-10">
                 <a
                     className="inline-block text-sm md:text-lg text-[#01f53f] px-2 py-3 md:py-3 md:px-5 border-[#01f53f] border-solid border rounded-md hover:bg-[#00701c] hover:border-[#00701c] hover:text-[#0B1120] transition ease-in-out duration-500"
-                    href="https://drive.google.com/file/d/16BYh3zo2oKybWUluRXHwWVtrBXHmM3Gw/view?usp=sharing"
+                    href="https://drive.google.com/file/d/17GnSe4FK9CV__Zd9-xu873pXoFXnuJQa/view?usp=sharing"
                     target="_blank"
                     rel="noreferrer"
                 >
